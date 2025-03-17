c2596ac4db51bf9076968a3fbdd0e0772feddc1a
Rob Panico <github@adtap.network>
2025-03-17 11:52:28 -0400

    1.0.5.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-import Malt from './classes/malt';
+import MaltInterface from './interfaces/malt';
+import AccountMaltInterface from './interfaces/malts/account';
+import AgentMaltInterface from './interfaces/malts/agent';
+import BlockMaltInterface from './interfaces/malts/block';
+import ErrorsMaltInterface from './interfaces/malts/errors';
+import FaultMaltInterface from './interfaces/malts/fault';
+import FiltersMaltInterface from './interfaces/malts/filters';
+import JsonMaltInterface from './interfaces/malts/json';
+import KeysMaltInterface from './interfaces/malts/keys';
+import MailerMaltInterface from './interfaces/malts/mailer';
+import MessageMaltInterface from './interfaces/malts/message';
+import ParamsMaltInterface from './interfaces/malts/params';
+import QueryMaltInterface from './interfaces/malts/query';
+import RouteMaltInterface from './interfaces/malts/route';
+import SectionMaltInterface from './interfaces/malts/section';
 
+export {
+    MaltInterface, AccountMaltInterface, AgentMaltInterface,
+    BlockMaltInterface, ErrorsMaltInterface, FaultMaltInterface,
+    FiltersMaltInterface, JsonMaltInterface, KeysMaltInterface,
+    MailerMaltInterface, MessageMaltInterface, ParamsMaltInterface,
+    QueryMaltInterface, RouteMaltInterface, SectionMaltInterface
+};
+
+import Malt from './classes/malt';
 import AccountMalt from './classes/malts/account';
 import AgentMalt from './classes/malts/agent';
 import AnalyticMalt from './classes/malts/analytic';
